2b41de31967eee2dfed9cbce6ae3a8d58f116993
juliovargas20 <jvdark2020@gmail.com>
2023-11-27 16:28:22 -0500

    Primer Commit

---
+++ alumnos.py
@@ -0,0 +1,9 @@
+class alumnos:
+    def __init__(self, nombres, apellidos, edad, nota, nacionalidad):
+        self.nombres = nombres
+        self.apellidos = apellidos
+        self.edad = edad
+        self.nota = nota
+        self.nacionalidad = nacionalidad
+
+    
